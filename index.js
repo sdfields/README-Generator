eb018ca6faf7d299c82cbbe959c7a38fa4abb711
sdfields <spencerdfields@gmail.com>
2022-12-20 15:25:40 -0600

    input successfully writes to a readme json need to make it generate file

--- index.js
+++ index.js
@@ -5,39 +5,39 @@ const contents = [
     {
         name: "Description",
         message: "Please type a description of your repository.",
-        type: input
+        type: "input",
     },
     {
         name: "Installation",
         message: "Please give a brief description of any necessary installations to properly run your project.",
-        type: input
+        type: "input",
     },
     {
         name: "Use",
         message: "Please describe how to use your application.",
-        type: input
+        type: "input",
     },
     {
         name: "Credits",
         message: "Please include a list of any collaborators.",
-        type: input
+        type: "input",
     },
     {
         name: "Links",
         message: "Please provide any useful links for your project.",
-        type: input
+        type: "input",
     },
     {
         name: "License",
         message: "Please include any licenses used.",
-        type: input
+        type: "input",
     },
 ];
 
 inquirer
   .prompt(contents)
-  .then((answers) =>
-    fs.writeFile('answers.json', JSON.stringify(answers), (error) => {
+  .then((readme) =>
+    fs.writeFile('readme.json', JSON.stringify(readme), (error) => {
         return error 
         ? console.error(error) 
         : console.log('README stored successfully!')
