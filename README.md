c80b97262c7177a46a15e49f1542d686f711dc13
sdfields <spencerdfields@gmail.com>
2023-01-02 04:45:52 -0600

    readme now transcribes to the readme file

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# README-Generator
+# Title
+gjh
+# Description
+hgj
+# Table of Contents
+gfhj
+# Installation
+gh
+# Use
+j
+# License
+ghj
+# Credits
+gh
+# Tests
+j
+# Questions
+ghfj
--- index.js
+++ index.js
@@ -17,6 +17,11 @@ const subjects = [
         type: "input",
     },
     {
+        name: "TableOfContents",
+        message: "Please create a table of contents.",
+        type: "input",
+    },
+    {
         name: "Installation",
         message: "Please give a brief description of any necessary installations to properly run your project.",
         type: "input",
@@ -27,37 +32,46 @@ const subjects = [
         type: "input",
     },
     {
+        name: "License",
+        message: "Please include any licenses used.",
+        type: "input",
+    },
+    {
         name: "Credits",
         message: "Please include a list of any collaborators.",
         type: "input",
     },
     {
-        name: "Links",
-        message: "Please provide any useful links for your project.",
+        name: "Tests",
+        message: "Please provide any tests for your project.",
         type: "input",
     },
     {
-        name: "License",
-        message: "Please include any licenses used.",
+        name: "Questions",
+        message: "Please provide any questions for your project.",
         type: "input",
     },
 ];
 
+function generateReadMe(answers) {
+    return "# Title\n" + answers.Title + "\n# Description\n" + answers.Description + "\n# Table of Contents\n" + answers.TableOfContents + "\n# Installation\n" + answers.Installation +"\n# Use\n" + answers.Use +"\n# License\n" + answers.License +"\n# Credits\n" + answers.Credits +"\n# Tests\n" + answers.Tests +"\n# Questions\n" + answers.Questions;
+}
+
 // TODO: Create a function to write README file
 // function writeToFile(README.md, data) {}
 
-inquirer
-  .prompt(subjects)
-  .then((readme) =>
-    fs.writeFile('readme.json', JSON.stringify(readme), (error) => {
-        return error 
-        ? console.error(error) 
-        : console.log('README stored successfully!')
-    })
-  );
-
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(subjects)
+    .then((answers) =>
+      fs.writeFile('README.md', generateReadMe(answers), (error) => {
+           return error 
+          ? console.error(error)
+          : console.log('README stored successfully!')
+      })
+    );
+}
 
 // Function call to initialize app
 init();
